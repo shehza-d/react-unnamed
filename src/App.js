8279f236127946bbea0ecd0e6e30a8f9dfc9ca8c
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-13 19:13:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header"> 
         <p>
-          Hello Shehzad.
+          Hello Shehzad
         </p>
       </header>
     </div>
